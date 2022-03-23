9738d365fa4cbff628879bb3d50d878ac23cdbf6
truli123 <99179711+truli123@users.noreply.github.com>
2022-03-23 21:29:37 +0100

    Delete SQLQuery8.sql

--- SQLQuery8.sql
+++
@@ -1,6 +0,0 @@
-SELECT * FROM dbo.Club
-SELECT * FROM dbo.Player
-INSERT INTO Club (ClubId, ClubName, ClubCity,ClubCountry,Stadium)
-
-	   VALUES (3,'Dinamo','Zagreb','Hrv', 'Maksimir');	
-	   DELETE FROM Club WHERE ClubId = 2
