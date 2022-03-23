6f3561e58847ae3ddcf7fbbd1bec7ce4fa85c511
DESKTOP-K7K3O10\Kreso <truli123@gmail.com>
2022-03-23 21:39:21 +0100

    SQL

---
+++ SQLQuery8.sql
@@ -0,0 +1,6 @@
+SELECT * FROM dbo.Club
+SELECT * FROM dbo.Player
+INSERT INTO Club (ClubId, ClubName, ClubCity,ClubCountry,Stadium)
+
+	   VALUES (3,'Dinamo','Zagreb','Hrv', 'Maksimir');	
+	   DELETE FROM Club WHERE ClubId = 2
